180ae05022862e4cc2b3da0df2912b5523621108
jevancousins <86753811+jevancousins@users.noreply.github.com>
2021-07-01 03:00:00 +0100

    Delete python directory

--- python/src/video_playlist.py
+++
@@ -1,9 +0,0 @@
-"""A video playlist class."""
-
-from .video_library import VideoLibrary
-
-class Playlist:
-    """A class used to represent a Playlist."""
-    def __init__(self, playlist_name:str):
-        self._video_library = VideoLibrary()
-
